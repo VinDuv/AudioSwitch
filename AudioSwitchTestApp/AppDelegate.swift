4216f8acb168a2de9701f09eb0f2542adb9f872e
Vincent Duvert <vincent@duvert.net>
2018-05-21 20:26:17 +0200

    Preference pane test application
    This test application loads the preference pane window controller, so it can be tested without creating a full preference pane.

---
+++ AudioSwitchTestApp/AppDelegate.swift
@@ -0,0 +1,10 @@
+// Copyright 2018 Vincent Duvert.
+// Distributed under the terms of the MIT License.
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    let prefs = PrefsWindowController.create()
+}
+
